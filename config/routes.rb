5567c1724be37ae607677af62f8346c06a438633
Herb Susmann <herbps10@gmail.com>
2014-01-02 22:22:29 -0500

    Finished adding routes

--- config/routes.rb
+++ config/routes.rb
@@ -57,4 +57,35 @@ Offcampus::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+
+  namespace :admin do
+       # Directs /admin/products/* to Admin::ProductsController
+       # (app/controllers/admin/products_controller.rb)
+    resources :landlords
+    resources :properties
+  end
+
+  # You can have the root of your site routed with "root"
+  # just remember to delete public/index.html.
+  # root :to => 'welcome#index'
+  root :to => 'pages#home'
+
+  match '/dashboard' => 'landlords#dashboard', :as => :user_root
+
+  # See how all your routes lay out with "rake routes"
+
+  # This is a legacy wild controller route that's not recommended for RESTful applications.
+  # Note: This route will make all actions in every controller accessible via GET requests.
+  #match ':controller(/:action(/:id))(.:format)'
+  match 'dashboard' => 'landlords#dashboard'
+
+  devise_scope :user do
+    get "sign_out", :to => "devise/sessions#destroy"
+  end
+
+  match '/list' => 'pages#list'
+
+  match '/landing' => 'pages#landing'
+
+  match '/js/properties' => 'properties#properties'
 end
