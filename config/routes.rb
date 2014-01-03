c68b15b70cd5cbbd8442558b3f90a17b8090b97c
Herb Susmann <herbps10@gmail.com>
2014-01-02 22:07:46 -0500

    Updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Offcampus::Application.routes.draw do
+  devise_for :users, :controllers => {:registrations => "registrations"}
+  resources :landlords
+  resources :properties
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
