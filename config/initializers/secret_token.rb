0dea2d26bde53ddb59892ae5f71730a4e21afb48
Elliott Regan <regan.elliott@gmail.com>
2014-01-02 21:16:26 -0500

    installed rails 4

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Offcampus::Application.config.secret_key_base = '95b0b54dbe8e58c8f6b244337e2a8d3faefa0d32ff3e70f65842f4605541b3fa3652aca3575e6a23bb3ce5784050a27ce6fdcc9d6dc85205e88c22f3a9b3eb29'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Offcampus::Application.config.session_store :cookie_store, key: '_offcampus_session'
