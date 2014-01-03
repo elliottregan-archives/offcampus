190e6268fc567c26f27ab35c625c8b46a1b760f7
Herb Susmann <herbps10@gmail.com>
2014-01-03 11:52:02 -0500

    stripped out unnecessary fields for properties (rooms, square footage)

--- app/models/property.rb
+++ app/models/property.rb
@@ -3,8 +3,6 @@ class Property < ActiveRecord::Base
 
 	validates :address, :presence => true
 	validates :description, :presence => true
-	validates :bedrooms, :presence => true, :numericality => { :only_integer => true }
-	validates :square_footage, :presence => true, :numericality => { :only_integer => true }
 	validates :price, :presence => true, :numericality => true
 
 	belongs_to :landlord
